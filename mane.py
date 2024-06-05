3fcbf53ddbde9c890efd440a824173d293240bfa
zerosergey2024 <rosinka_ekt@mail.ru>
2024-06-05 08:38:08 +0300

    Update mane.py

--- mane.py
+++ mane.py
@@ -1,1 +1,30 @@
-a = 5
+class Animal():
+    def make_saund(self):
+        pass
+class Bird(Animal):
+    def __init__ (self, sparrowe):
+        self.sparrow = sparrowe
+    def make_saund(selfself):
+        print("song")
+
+class Mammal(Animal):
+    def __init__ (self, cow):
+        self.cow = cow
+    def make_saund(self):
+        print("muu")
+
+class Reptile(Animal):
+    def __init__ (self, crocodile):
+        self.crocodile = crocodile
+    def make_saund(self):
+        print("roar")
+
+    sparrow = Bird("sparrow")
+    cow = Mammal("cow")
+    crocodile = Reptile("crocodile")
+
+    animals = [sparrow, cow, crocodile]
+
+    for animal in animals:
+        animal.make_saund()
+
