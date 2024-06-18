e422937f73888d1c533a9b68b68561aada8c5f30
zerosergey2024 <rosinka_ekt@mail.ru>
2024-06-18 19:54:21 +0300

    add code

--- mane.py
+++ mane.py
@@ -1,1 +1,14 @@
-a = 5
+#- Создайте таблицу `person` с пятью столбцами:
+#- `id` — идентификатор человека, целое число, первичный ключ.
+#- `first_name` — имя человека, строка.
+#- `last_name` — фамилия человека, строка.
+#- `age` — возраст человека, целое число.
+#- `email` — электронная почта человека, строка.
+
+CREATE TABLE person (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age INTEGER,
+    email VARCHAR(50)
+)
