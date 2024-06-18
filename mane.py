8ea6d09c6f6bedda83de3d2ad71024dc1f7c176c
zerosergey2024 <rosinka_ekt@mail.ru>
2024-06-18 21:05:32 +0300

    add code

--- mane.py
+++ mane.py
@@ -1,14 +1,26 @@
 #- Создайте таблицу `person` с пятью столбцами:
-#- `id` — идентификатор человека, целое число, первичный ключ.
-#- `first_name` — имя человека, строка.
-#- `last_name` — фамилия человека, строка.
-#- `age` — возраст человека, целое число.
-#- `email` — электронная почта человека, строка.
 
-CREATE TABLE person (
+CREATE TABLE "person" (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     age INTEGER,
     email VARCHAR(50)
-)
+);
+#Заполняем таблицу данными на 6 строк:
+
+INSERT INTO "person" ("id", "first_name", "last_name", "age", "email")
+VALUES    (1, 'Катя', 'Иварова', 30, '3@gmail.com'),
+VALUES    (2, 'Коля', 'Сидоров', 25, '3@gmail.com'),
+VALUES    (3, 'Кирил', 'Петров', 40, '3@gmail.com'),
+VALUES    (4, 'Ксюша', 'Данилова', 35,'3@gmail.com'),
+VALUES    (5, 'Костя', 'Васильев', 30,'3@gmail.com'),
+VALUES    (6, 'Клава', 'Сергеева', 25,'3@gmail.com')
+
+#Переименование таблицы `person` в `employee`
+
+ALTER TABLE "person" RENAME  TO "employee";
+
+#Удалите из таблицы `employee` столбец `email`:
+
+ALTER TABLE "employee" DROP COLUMN "email";
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-print("hello world")
-
-print("test")
-
-print("демонстрация")
+CREATE TABLE "person", (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    age INTEGER,
+    email VARCHAR(50)
+);
